74a1269f02e26e0da31202552a1eaeeaa1d94f28
Saurav Kumar <67797045+rjsaurav13@users.noreply.github.com>
2021-01-30 23:39:02 +0530

    Update and rename Program4.java to DataType_Input.java

--- Hacker Rank/DataType_Input.java
+++ Hacker Rank/DataType_Input.java
@@ -6,12 +6,9 @@ public class Program4 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a;
-		double b;
-		String c;
-		a = scan.nextInt();
-		b = scan.nextDouble();
-		c = scan.nextLine();
+		int a = scan.nextInt();
+		double b = scan.nextDouble();
+		String c = scan.nextLine();
 		c=scan.nextLine();
 		System.out.println("String: " + c);
 		System.out.println("Double: " + b);
